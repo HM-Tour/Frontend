ffff8cf33f3c77a67d82b9d2da26091eaab57ad4
Ibrahim Almanaseer <ibrahimnalmanaseer@gmail.com>
2023-01-23 09:41:01 +0000

    last updated

--- app/page.jsx
+++ app/page.jsx
@@ -8,11 +8,11 @@ import PostSummary from "./components/PostSummary";
 import { useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({ }) {
+export default function Home({}) {
   const [data, setData] = useState(null);
 
 
-
+  
 
   function handleUpdate(post) {
     setData(post);
@@ -24,7 +24,8 @@ export default function Home({ }) {
   // const {tokens} = useContext(AuthContext)
   return (
     <>
-
+     <div className="container mx-auto px-10 mb-8">
+        <PostSummary/>
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 ">
           <div className="lg:col-span-8 col-span-1">
             {/* <Header/> */}
